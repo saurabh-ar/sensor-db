ad77cf6c2852c721df86933aa0a863564365e38d
rjha <jha.rajeev@gmail.com>
2013-08-28 19:13:59 +0530

    script to send sensor data coming over serial port to  yuktix

---
+++ arduino/data-point.py
@@ -0,0 +1,29 @@
+import requests
+import json
+import sys
+import time 
+import serial
+
+def post_data_point (value) :
+    url = "http://yuktix01.cloudapp.net:8080/sensordb/v1/datapoint"
+    unix_ts = int(time.time())
+    data = { 
+        'projectId':'p001', 
+        'serialNumber': 's001', 
+        'readings': [ {"name" : "resistor" , "value" : value , "timestamp" : unix_ts} ] 
+        }
+
+    post_data = json.dumps(data)
+    post_data = post_data + "\r\n"
+    headers = { 'Content-type': 'application/json' }
+    r = requests.post(url, data=post_data, headers=headers)
+    print r.status_code
+
+    return 
+
+ser = serial.Serial('/dev/tty.usbserial', 9600)
+while True:
+    value = ser.readline()
+    post_data_point(value)
+
+# sys.exit(0)
